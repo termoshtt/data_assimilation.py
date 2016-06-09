5dd2dc45940881b0347c3e966cf7b643d23548c6
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-06-09 18:26:29 +0900

    Add integrator of Lorenz 96 model

---
+++ DA/Lorenz96.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+
+
+def Lorenz96(F):
+    def f(x):
+        return (np.roll(x, -1) - np.roll(x, 2)) * np.roll(x, 1) - x + F
+    return f
+
+
+def RK4(f, dt):
+    def teo(x):
+        k1 = dt*f(x)
+        k2 = dt*f(x+k1/2)
+        k3 = dt*f(x+k2/2)
+        k4 = dt*f(x+k3)
+        return x + (k1+2*k2+2*k3+k4)/6
+    return teo
+
+
+def Lorenz96_RK4(F, dt):
+    return RK4(Lorenz96(F), dt)
