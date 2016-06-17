6c0a676430ae6a7012c92d913ce744a523e957c8
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-06-17 12:32:50 +0900

    Add noisy observation

--- DA/observation.py
+++ DA/observation.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 from scipy.sparse.linalg import LinearOperator
 
 
@@ -11,3 +12,8 @@ def trivial(N):
 def head(N, p):
     """ Observe first p data """
     return LinearOperator((p, N), matvec=lambda x: x[:p])
+
+
+def add_noise(H, intensity):
+    _, N = H.shape
+    return lambda x: H(x) + intensity*np.random.normal(size=N)
