4b0918e70e0db31f645cf6560135d9d7697696a1
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-06-17 12:33:18 +0900

    Add make_init, evaluate_rms in Lorenz96

--- DA/Lorenz96.py
+++ DA/Lorenz96.py
@@ -2,6 +2,7 @@
 
 
 import numpy as np
+from . import ensemble
 
 
 def Lorenz96(F):
@@ -24,12 +25,37 @@ def Lorenz96_RK4(F, dt):
     return RK4(Lorenz96(F), dt)
 
 
-def teo(F, T, n_step):
-    dt = T / n_step
-    U = Lorenz96_RK4(F, dt)
+def forcast(F, dt):
+    return ensemble.forcast(Lorenz96_RK4(F, dt))
 
-    def f(x):
-        for _ in range(n_step):
+
+def make_init(N, F, dt, T):
+    teo = Lorenz96_RK4(F, dt)
+    x = np.sin(np.arange(0, np.pi, np.pi/N))
+    for t in range(T):
+        x = teo(x)
+    return x
+
+
+def assimilation(F, dt, A, obs, K, T, init_noise=1):
+    U = Lorenz96_RK4(F, dt)
+    F = forcast(F, dt)
+
+    def da(x):
+        xa = x.copy()
+        Xa = ensemble.make_ensemble(len(x), K, init_noise)
+        for t in range(T):
             x = U(x)
-        return x
-    return f
+            xb, Xb = F(xa, Xa)
+            xa, Xa = A(xb, Xb, obs(x))
+            yield x, xa, Xa
+    return da
+
+
+def evaluate_rms(N, F, dt, A, obs, K, T, init_noise=1):
+    x = make_init(N, F, dt, T)
+    da = assimilation(F, dt, A, obs, K, T, init_noise)
+    rms_sum = 0
+    for x, xa, _ in da(x):
+        rms_sum += np.linalg.norm(x-xa) / np.sqrt(len(x))
+    return rms_sum / T
