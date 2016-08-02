31a46ed79d0bcc8422caf11dc8eec2dbf0a6b11b
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-08-02 14:51:43 +0900

    Bug fix (passed)

--- DA/test_lyapunov.py
+++ DA/test_lyapunov.py
@@ -3,6 +3,7 @@
 from . import lyapunov, model
 
 import numpy as np
+from numpy.testing import assert_allclose, assert_equal
 from unittest import TestCase
 
 
@@ -19,18 +20,18 @@ class TestLyapunov(TestCase):
         a = np.random.random(3)
         b = np.random.random(3)
         J = lyapunov.Jacobi(self.U, self.x)
-        np.testing.assert_array_almost_equal(J(a+b), J(a)+J(b))
+        assert_allclose(J(a+b), J(a)+J(b), atol=1e-6)
 
     def test_jacobi_nonsquare_matrix(self):
         A = np.random.random((3, 2))
         J = lyapunov.Jacobi(self.U, self.x)
-        np.testing.assert_equal(J(A).shape, (3, 2))
+        assert_equal(J(A).shape, (3, 2))
 
     def test_jacobi_matrix_assoc(self):
         A = np.random.random((3, 3))
         B = np.random.random((3, 3))
         J = lyapunov.Jacobi(self.U, self.x)
-        np.testing.assert_almost_equal(np.dot(J(A), B), J(np.dot(A, B)))
+        assert_allclose(np.dot(J(A), B), J(np.dot(A, B)), atol=1e-7)
 
     def test_clv_forward(self):
         T = 1000
@@ -43,9 +44,9 @@ class TestLyapunov(TestCase):
             Qn = nex["Q"]
             Rn = nex["R"]
             J = lyapunov.Jacobi(self.U, x)
-            np.testing.assert_allclose(J(Q), np.dot(Qn, Rn))
+            assert_allclose(J(Q), np.dot(Qn, Rn), atol=1e-7)
 
-    def test_clv_backward_C(self):
+    def test_clv_backward_QR(self):
         T = 1000
         tl = lyapunov._clv_forward(self.U, self.x, T)
         tl = lyapunov._clv_backward(tl)
@@ -54,22 +55,16 @@ class TestLyapunov(TestCase):
             nex = tl[t+1]
             C = np.dot(now["Q"].T, now["V"])
             Cn = np.dot(nex["Q"].T, nex["V"])
+            Q = now["Q"]
+            Qn = nex["Q"]
             Rn = nex["R"]
-            D = nex["D"]
-            RCD = lyapunov.rescaled(np.dot(Rn, C), D)
-            np.testing.assert_almost_equal(RCD, Cn)
-
-    def test_clv_backward_V(self):
-        T = 10
-        tl = lyapunov._clv_forward(self.U, self.x, T)
-        tl = lyapunov._clv_backward(tl)
-        for t in range(T-1):
-            now = tl[t]
-            nex = tl[t+1]
-            x = now["x"]
-            J = lyapunov.Jacobi(self.U, x)
             V = now["V"]
             Vn = nex["V"]
             Dn = nex["D"]
-            JVD = lyapunov.rescaled(J(V), Dn)
-            np.testing.assert_allclose(JVD, Vn)
+            x = now["x"]
+            J = lyapunov.Jacobi(self.U, x)
+            assert_allclose(np.dot(J(Q), C), J(np.dot(Q, C)), atol=1e-6)
+            assert_allclose(np.dot(np.dot(Qn, Rn), C), J(V), atol=1e-6)
+            assert_allclose(lyapunov.rescaled(np.dot(Rn, C), Dn),
+                            Cn, atol=1e-8)
+            assert_allclose(lyapunov.rescaled(J(V), Dn), Vn, atol=1e-6)
